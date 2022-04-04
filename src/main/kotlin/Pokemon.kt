2e120040f59960a2a8732ee75d4a69f95aebfabe
Giovani Rodrigues <giovani.srodrigues@hotmail.com>
2022-04-04 20:42:17 -0300

    Pokemon Class

---
+++ src/main/kotlin/Pokemon.kt
@@ -0,0 +1,37 @@
+open class Pokemon(var especie: String) {
+    var level = (0..100).random()
+    open var nome: String = "Pokemon"
+    var ataque = level * 5
+    var vida = level * 5
+
+    fun atacar(): Boolean {
+        var ataqueEfetivo = ataque * 1.3
+        vida - ataqueEfetivo
+
+        println("${nome} perdeu ${ataqueEfetivo} pontos de vida")
+
+        if (vida <= 0) {
+            println("${nome} foi derrotado")
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+class PokemonEletrico : Pokemon("Eletrico") {
+    val opcoesPokemonEletrico = listOf<String>("Zeraora", "Pikachu", "Helioptile", "Tynamo")
+    override var nome = opcoesPokemonEletrico.random()
+}
+
+class PokemonFogo : Pokemon("Fogo") {
+    val opcoesPokemonFogo = listOf<String>("Growlithe", "Vulpix", "Oricorio", "Charmander")
+    override var nome = opcoesPokemonFogo.random()
+}
+
+class PokemonAgua : Pokemon("Agua") {
+    val opcoesPokemonAgua = listOf<String>("Psyduck", "Blastoise", "Poliwhirl", "Poliwag")
+    override var nome = opcoesPokemonAgua.random()
+}
+
+
