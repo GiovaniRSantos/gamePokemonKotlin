a0492c61aae6fc206ff465081062e670db390160
Giovani Rodrigues <giovani.srodrigues@hotmail.com>
2022-04-04 23:29:09 -0300

    funcoes de dinheiro

--- src/main/kotlin/Pessoa.kt
+++ src/main/kotlin/Pessoa.kt
@@ -1,10 +1,11 @@
-class Pessoa {
+class Pessoa(var dinheiro: Int) {
     val nomeJogador = readLine()!!
     var nome = nomeJogador
     lateinit var pokemonSelecionado: String
+    var pokemons = mutableListOf<String>()
 
 
-    fun escolherPokemon() {
+    fun selecionarPokemon() {
         var pokemonEletrico = PokemonEletrico()
         var pokemonFogo = PokemonAgua()
         var pokemonAgua = PokemonFogo()
@@ -32,16 +33,45 @@ class Pessoa {
             }
             else -> {
                 println("Para prosseguir, você precisa escolher um Pokemon... Reiniciando processo de escolha de Pokemon.")
-                var retry = this.escolherPokemon()
+                var retry = this.selecionarPokemon()
                 retry
             }
         }
+        pokemons.add(pokemonSelecionado)
 
+    }
+
+    fun mostrarPokemons(): MutableList<String> {
+        pokemons.forEach { println("Estes são seus pokemons: ${pokemons}.") }
+        return pokemons
+    }
+
+    fun escolherPokemon(pokemon: String): Any {
+        if (pokemon in pokemons) {
+            println("${nome} escolheu o pokemon ${pokemon}.")
+            return pokemon
+        } else {
+            return println("ERRO: Pokemon não encontrado.")
+        }
+    }
+
+    fun mostrarDinheiro() {
+        println("Você possui $${dinheiro} em sua conta.")
+    }
+
+    fun ganharDinheiro(quantidade: Int) {
+        dinheiro = +quantidade
+        print("Você ganhou $${quantidade}.\n")
+        this.mostrarDinheiro()
     }
 }
 
 
 fun iniciarPessoa() {
-    var pessoa = Pessoa()
-    pessoa.escolherPokemon()
+    var pessoa = Pessoa(0)
+    pessoa.selecionarPokemon()
+    pessoa.mostrarPokemons()
+    var escolherPokemon = readLine()!!
+    pessoa.escolherPokemon(escolherPokemon)
+    pessoa.ganharDinheiro(100)
 }
